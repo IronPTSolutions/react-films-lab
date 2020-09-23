9f0d5f4ce3c6a9eaee50cff33c7a10aa3f1c01d1
miguelbachi5a <miguelbachi5a@gmail.com>
2020-09-23 09:12:36 +0200

    Yellow when high rate

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ function Card(props) {
     const listOfFilms = props.films.map(film => {
         const averageRate = (film.ratings.reduce((a, b) => (a + b), 0) / film.ratings.length).toFixed(1);
         return (
-            <li className="Card" key={props.id}>
+            <li className={`Card ${averageRate >= 6 && 'yellow'}`} key={props.id}>
                 <ImageDiv posterurl={film.posterurl} genre={film.genres}/>
                 <Info title={film.title} storyline={film.storyline} />
                 <Footer year={film.year} averageRate={averageRate} />

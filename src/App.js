df04a2338602129e0717fec5260903066033e3f7
Andrea Astorga <andrea.astorga95@gmail.com>
2020-09-25 20:17:47 +0200

    Exercise done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Movie from './components/Movie.jsx';
+import allMovies from './data/films.json';
+import Nav from './components/Nav.jsx'
 
-const App = () => (
-  <div className="App">
-    {/* Your code here */}
-  </div>
-)
+class App extends React.Component {
+  state = {
+    movies: allMovies
+  }
+
+  deleteFilm = (e) => {
+    const allMovies = [...this.state.movies];
+    const movieId = e.target.parentNode.id;
+    const filteredMovies = allMovies.filter(m => m.id !== movieId)
+    //console.log(movie.id)
+    this.setState({movies: filteredMovies})
+
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <div className="" style={{marginLeft: '2rem'}}>
+          <div className="">
+            <div className="card-columns">
+
+                {this.state.movies.map(movie => {
+                  return <Movie {...movie} deleteMovie={this.deleteFilm} key={movie.id}/>
+                  
+                })}
+
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+  
+
 
 export default App

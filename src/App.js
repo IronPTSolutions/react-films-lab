33c4d32a5fd129d1873083afce609cd9ff2c4ce0
Andres Martinez <andresmartgonz@gmail.com>
2020-09-27 14:20:39 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from 'react'
+import FilmCard from './components/FilmCard'
 
 const App = () => (
   <div className="App">
-    {/* Your code here */}
+    <div className="container-fluid banner">
+      <div className="container">
+        <div className="col-12">
+          <h1>Iron<strong>Films</strong></h1>
+        </div>
+      </div>
+    </div>
+    <div className="container movies-info">
+        <FilmCard />
+    </div>
   </div>
 )
 

752255b3dac73006713ecb165ac6406133975c3b
miguelbachi5a <miguelbachi5a@gmail.com>
2020-12-06 19:00:09 +0100

    fix: Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import ImageDiv from './ImageDiv';
-import Info from './Info';
-import Footer from './Footer';
-import './Card.css';
+import React, { useState } from 'react'
+import ImageDiv from './ImageDiv'
+import Info from './Info'
+import Footer from './Footer'
+import './Card.css'
 
 function Card(props) {
 
-    const [allFilms, setAllFilms] = useState(props.films);
+    const [allFilms, setAllFilms] = useState(props.films)
     const deleteFilm = (filmToDelete) =>  {
         const filtered = allFilms.filter(film => {
            return film.title !== filmToDelete.title
         })
         setAllFilms(filtered)
-    };
+    }
 
     const listOfFilms = allFilms.map(film => {
         const averageRate = (film.ratings.reduce((a, b) => (a + b), 0) / film.ratings.length).toFixed(1);
@@ -24,13 +24,13 @@ function Card(props) {
                 <Footer year={film.year} averageRate={averageRate} />
             </li>
         )
-    });
+    })
 
     return (
         <ul>
             {listOfFilms}
         </ul>
-    );
-};
+    )
+}
 
-export default Card;
+export default Card

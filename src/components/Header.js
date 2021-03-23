fb8fb4d7fab544d9d9c98dee82a16b447e573572
monteromario <59968975+monteromario@users.noreply.github.com>
2021-03-23 20:18:29 +0100

    Done

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="Header container py-4 px-4">
+            <div className="row">
+                <div className="col">
+                    <h1 className="fw-light mb-3 display-4">Iron Films</h1>
+                    <h4 className="fw-lighter">React component state tutorial</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header

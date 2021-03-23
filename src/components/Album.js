7c1fc7bd3e94cf47ddc61f1f51021846d307209f
rgarsan <rgarsan84@gmail.com>
2021-03-23 21:37:58 +0100

    done??

--- src/components/Album.js
+++ src/components/Album.js
@@ -8,18 +8,25 @@ class Album extends Component{
         films: [...data]//creamos copia de la array con el spread
     }
 
+    deleteFilms = (id)=>{
+        this.setState({
+            films:this.state.films.filter(film => film.id !== id)//Cambiamos el estado de films, lo filtramos y nos quedamos con aquello que el id sea diferente al id que le pasamos en el metodo.
+
+        })
+    }
+
 
     render() {
         return(
            
-         <div className="Album">
+         <div className="Album ">
             <div className="container ">
-                <div className= "row row-cols-3 " >
+                <div className= "row row-cols-3   " >
                 
                     {
-                        this.state.films.map((film)=> ( // Mapeamos el array y sacamos el título.
+                        this.state.films.map((film)=> ( // Mapeamos el array y lo buscamos por el id
                             <div className="col" key={film.id}>
-                                <AlbumItem  {...film}/>
+                                <AlbumItem  {...film} onDelete={this.deleteFilms}/>
 
                             </div>
                         ))
--- src/components/AlbumItem.js
+++ src/components/AlbumItem.js
@@ -1,24 +1,37 @@
 import React from 'react'
+import './AlbumItems.css'
 
 
-const AlbumItem = ({title,storyline,posterurl,year,imdbRating})=>{
+const AlbumItem = ({title,storyline,posterurl,year,imdbRating,onDelete,id,genres})=>{
     return(
 
-        <div className=" Card card " >
-       
-        <img src={posterurl} className="card-img-top" alt={title}/>
-        <div className="card-body">
-         
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{storyline}</p>
-            <button type="button" className="btn-close" aria-label="Close"></button>
-            <div className="card-footer">
-             {year} {imdbRating}
+        <div className=" Card card  " >
+                <button onClick={() => onDelete(id)} className="btn-close btn-danger  button"       aria-label="Close"></button>
+               
+               <img src={posterurl} className="card-img-top" alt={title}/>
+
+               <div className="genres">
+                    <spam className= "gen1">{genres[0]}</spam>
+                    <spam className= "gen2">{genres[1]}</spam>
+                    <spam className= "gen3">{genres[2]}</spam>
+                </div>
+                
+                
+                
+                <div className="card-body">
+                
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{storyline}</p>
+                
+                    <div className="card-footer footer justify-content-betwee">
+                        <div className="year">{year}</div>
+                        <div className="rating">{imdbRating}</div>
+                    
              
             
-            </div>
+                    </div>
+                </div>
         </div>
-</div>
     )
 }
 
